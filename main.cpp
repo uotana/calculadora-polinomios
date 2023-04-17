646f0c2e0b247165294f87362c1f49eebf6d823c
uotana <uotanacarvalho@gmail.com>
2023-04-17 20:05:13 -0300

    Adiciona main (completo)

--- main.cpp
+++ main.cpp
@@ -1,143 +1,152 @@
 #include <iostream>
 #include <string>
+
 #include "Poly.h"
 
 using namespace std;
 
+/// Funcao auxiliar para imprimir informacao
+/// sobre o grau do polinomio
+string printGrau(const Poly& P)
+{
+    string prov;
+
+    if (!P.empty()) prov=" ";
+    prov += '[';
+    if (P.empty()) prov += "Vazio";
+    else if (P.isZero()) prov += "Nulo";
+    else
+    {
+        prov += "Grau ";
+        prov += to_string(P.getGrau());
+    }
+    prov += ']';
+    return prov;
+}
+
 int main(void)
 {
-    Poly Pvaz;
-    Poly Pnulo(0);
-    Poly P2A(2), P2B(2);
-    Poly P3(3);
-    Poly Result;
+    Poly P1;
+    Poly P2(0);
+    Poly Result(1);
+    double x;
+    int grau;
+    char opcao;
 
-    // Polinomios para teste
-    P2A.setCoef(2,-3.0);
-    P2A.setCoef(1,5.0);
-    P2A.setCoef(0,6.0);
-
-    P2B.setCoef(2,3.0);
-    P2B.setCoef(1,-5.0);
-    P2B.setCoef(0,-1.0);
-
-    P3.setCoef(3,1.0);
-    P3.setCoef(2,-3.0);
-    P3.setCoef(1,5.0);
-    P3.setCoef(0,6.0);
-
-    // Testando negativo de vazio
-    Result = -Pvaz;
-    if (Result.empty()) cout << "OK -vazio\n";
-    else cerr << "ERRO -vazio\n";
-
-    // Testando negativo de nulo
-    Result = -Pnulo;
-    if (Result.isZero()) cout << "OK -nulo\n";
-    else cerr << "ERRO -nulo\n";
-
-    // Testando negativo de polinomio
-    Result = -P2A;
-    try
+    cout << "CALCULADORA DE POLINOMIOS\n";
+    // Leh os valores da execucao anterior
+    // Se nao existirem os arquivos, nao faz nada
+    P1.ler("poly_P1.txt");
+    P2.ler("poly_P2.txt");
+    Result.ler("poly_result.txt");
+    do
     {
-        if (Result.getGrau() != P2A.getGrau()) throw 1;
-        for (int i=0; i<=Result.getGrau(); ++i) if (Result.getCoef(i) != -P2A.getCoef(i)) throw 2;
-        cout << "OK -polinomio\n";
+        // Solicita a opcao
+        do
+        {
+            cout << endl;
+
+            cout << "P1:     " << P1 << printGrau(P1) << endl;
+            cout << "P2:     " << P2 << printGrau(P2) << endl;
+            cout << "Result: " << Result << printGrau(Result) << endl;
+
+            cout << "Digite sua opcao:\n";
+            cout << "< - Entrar um novo polinomio\n";
+            cout << "+ - Somar os polinomios\n";
+            cout << "- - Subtrair os polinomios\n";
+            cout << "* - Multiplicar os polinomios\n";
+            // Opcionais
+            /*
+            cout << "/ - Dividir os polinomios (retornar quociente)\n";
+            cout << "% - Dividir os polinomios (retornar resto)\n";
+            */
+            cout << "x - Calcular o ultimo polinomio (Result) para um valor de x\n";
+            cout << "i - Inverter o sinal do ultimo polinomio (Result)\n";
+            cout << "t - Trocar os polinomios (P1->P2 P2->Result Result->P1)\n";
+            cout << "q - Terminar\n";
+            cin >> opcao;
+            opcao = tolower(opcao);
+        }
+        while (opcao!='<' && opcao!='+' && opcao!='-' && opcao!='*' &&
+               // Opcionais
+               /*
+               opcao!='/' && opcao!='%' &&
+               */
+               opcao!='x' && opcao!='i' && opcao!='t' && opcao!='q');
+
+        // Rotaciona os polinomios no buffer
+        if (opcao!='i' && opcao!='q') // Nao rotaciona nada se for i,q
+        {
+            if (opcao=='t') // Rotaciona no sentido inverso
+            {
+                Poly prov(Result);
+                Result = P2;
+                P2 = P1;
+                P1 = prov;
+            }
+            else // Todas as opcoes menos i,t,q: rotaciona normalmente
+            {
+                P1 = P2;
+                P2 = Result;
+                // Novo resultado entra em result
+            }
+        }
+
+        // Executa a opcao escolhida
+        switch(opcao)
+        {
+            case '<':
+                cout << "Grau do polinomio: "  ;
+                cin >> grau;
+                Result.recriar(grau);
+                if (grau >= 0)
+                {
+                    cout << "Coeficientes do polinomio:\n";
+                    cin >> Result;
+                }
+                break;
+            case '+':
+                Result = P1+P2;
+                break;
+            case '-':
+                Result = P1-P2;
+                break;
+            case '*':
+                Result = P1*P2;
+                break;
+
+                // Opcionais
+                /*
+                case '/':
+                  Result = P1/P2;
+                  break;
+                case '%':
+                  Result = P1%P2;
+                  break;
+                */
+
+            case 'x':
+                cout << "Valor de x: ";
+                cin >> x;
+                Result.recriar(0);
+                Result.setCoef(0,P2(x));
+                break;
+            case 'i':
+                Result = -Result;
+                break;
+            case 't':
+            case 'q':
+                break;
+            default:
+                cerr << "Opcao desconhecida\n";
+                Result=Poly();
+                break;
+        }
+        P1.salvar("poly_P1.txt");
+        P2.salvar("poly_P2.txt");
+        Result.salvar("poly_result.txt");
     }
-    catch(int i)
-    {
-        cerr << "ERRO -polinomio: -[" << P2A << "]=" << Result << endl;
-    }
-
-    // Testando soma de vazio e vazio
-    Result = Pvaz+Pvaz;
-    if (Result.empty()) cout << "OK vazio+vazio\n";
-    else cerr << "ERRO vazio+vazio\n";
-    // Testando subtracao de vazio e vazio
-    Result = Pvaz-Pvaz;
-    if (Result.empty()) cout << "OK vazio-vazio\n";
-    else cerr << "ERRO vazio-vazio\n";
-
-    // Testando soma de vazio e nulo
-    Result = Pvaz+Pnulo;
-    if (Result.isZero()) cout << "OK vazio+nulo\n";
-    else cerr << "ERRO vazio+nulo\n";
-    // Testando subtracao de vazio e nulo
-    Result = Pvaz-Pnulo;
-    if (Result.isZero()) cout << "OK vazio-nulo\n";
-    else cerr << "ERRO vazio-nulo\n";
-
-    // Testando soma de nulo e vazio
-    Result = Pnulo+Pvaz;
-    if (Result.isZero()) cout << "OK nulo+vazio\n";
-    else cerr << "ERRO nulo+vazio\n";
-    // Testando subtracao de nulo e vazio
-    Result = Pnulo-Pvaz;
-    if (Result.isZero()) cout << "OK nulo-vazio\n";
-    else cerr << "ERRO nulo-vazio\n";
-
-    // Testando soma de polinomio e vazio
-    Result = P2A+Pvaz;
-    if (Result == P2A) cout << "OK polinomio+vazio\n";
-    else cerr << "ERRO polinomio+vazio\n";
-    // Testando subtracao de polinomio e vazio
-    Result = P2A-Pvaz;
-    if (Result == P2A) cout << "OK polinomio-vazio\n";
-    else cerr << "ERRO polinomio-vazio\n";
-
-    // Testando soma de vazio e polinomio
-    Result = Pvaz+P2A;
-    if (Result == P2A) cout << "OK vazio+polinomio\n";
-    else cerr << "ERRO polinomio+vazio\n";
-    // Testando subtracao de vazio e polinomio
-    Result = Pvaz-P2A;
-    if (Result == -P2A) cout << "OK vazio-polinomio\n";
-    else cerr << "ERRO polinomio-vazio\n";
-
-    // Testando soma de polinomio2 e polinomio3
-    Result = P2A+P3;
-    if (Result.getGrau() != 3) cerr << "ERRO no grau de polinomio2+polinomio3\n";
-    cout << P2A << " + " << P3 << " = " << Result << " (verifique se estah correto)\n";
-    // Testando subtracao de polinomio2 e polinomio3
-    Result = P2A-P3;
-    if (Result.getGrau() != 3) cerr << "ERRO no grau de polinomio2-polinomio3\n";
-    cout << P2A << " - " << P3 << " = " << Result << " (verifique se estah correto)\n";
-
-    // Testando soma de polinomio3 e polinomio2
-    Result = P3+P2A;
-    if (Result.getGrau() != 3) cerr << "ERRO no grau de polinomio3+polinomio2\n";
-    cout << P3 << " + " << P2A << " = " << Result << " (verifique se estah correto)\n";
-    // Testando subtracao de polinomio3 e polinomio2
-    Result = P3-P2A;
-    if (Result.getGrau() != 3) cerr << "ERRO no grau de polinomio3-polinomio2\n";
-    cout << P3 << " - " << P2A << " = " << Result << " (verifique se estah correto)\n";
-
-    // Testando soma de polinomio2 e polinomio2 com reducao de grau
-    Result = P2A+P2B;
-    cout << P2A << " + " << P2B << " = " << Result << '\t';
-    if (Result.getGrau() != 0) cerr << "ERRO no grau de polinomio2+polinomio2 com reducao\n";
-    else if (Result.getCoef(0) != 5.0) cerr << "ERRO no coeficiente de polinomio2+polinomio2 com reducao\n";
-    else cout << "OK polinomio2+polinomio2 com reducao\n";
-
-    // Testando subtracao de polinomio2 e polinomio2 com reducao de grau
-    Result = P2B - (-P2A);
-    cout << P2B << " - " << -P2A << " = " << Result << '\t';
-    if (Result.getGrau() != 0) cerr << "ERRO no grau de polinomio2-polinomio2 com reducao\n";
-    else if (Result.getCoef(0) != 5.0) cerr << "ERRO no coeficiente de polinomio2-polinomio2 com reducao\n";
-    else cout << "OK polinomio2-polinomio2 com reducao\n";
-
-    // Testando soma de polinomio2 e polinomio2 com resultado nulo
-    Result = P2A + (-P2A);
-    cout << P2A << " + " << -P2A << " = " << Result << '\t';
-    if (!Result.isZero()) cerr << "ERRO polinomio2+polinomio2 com resultado nulo\n";
-    else cout << "OK polinomio2+polinomio2 com resultado nulo\n";
-
-    // Testando subtracao de polinomio2 e polinomio2 com resultado nulo
-    Result = P2B - P2B;
-    cout << P2B << " - " << P2B << " = " << Result << '\t';
-    if (!Result.isZero()) cerr << "ERRO polinomio2-polinomio2 com resultado nulo\n";
-    else cout << "OK polinomio2-polinomio2 com resultado nulo\n";
+    while (opcao != 'q');
 
     return 0;
 }
-
